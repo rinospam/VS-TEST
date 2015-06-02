0ab8f8c796ac6ce53a92c23f646db4eb8b7f8beb
rinospam <rino_spam@hotmail.com>
2015-06-02 19:20:14 +0700

    dynamic_type
    Signed-off-by: rinospam <rino_spam@hotmail.com>

---
+++ dynamic_type/dynamic_type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_type
+{
+    class Program
+    {
+        private static void M(Int32 n) { Console.WriteLine("M Int32:" + n); Console.ReadLine(); }
+        private static void M(string n) { Console.WriteLine("M string:" + n); Console.ReadLine(); }
+        static void Main(string[] args)
+        {
+            dynamic value;
+            for (Int32 demo = 0; demo < 2; demo++)
+            {
+                value = (demo == 0) ? (dynamic)5 : (dynamic)"А";
+                value = value + value;
+                M(value);
+                
+            }
+        }
+    }
+
+}
---
+++ dynamic_type/dynamic_type/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("dynamic_type")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dynamic_type")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("202eb604-594c-48f8-8255-71ce5a9a4f74")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
