19caa2ed6ecc1f3c3fa83f84cdd16eccac2272fb
zakharovrv roman <rino_spam@hotmail.com>
2015-06-05 10:38:03 +0700

    override
    Signed-off-by: zakharovrv roman <rino_spam@hotmail.com>

---
+++ override/override/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal struct Point 
+    {
+        private Int32 m_x, m_y;
+        public Point (Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        public void Change(Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", m_x.ToString(), m_y.ToString());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(1, 1);
+            Console.WriteLine(p);
+
+            p.Change(2, 2);
+            Console.WriteLine(p);
+
+            Object o = p;
+            Console.WriteLine(o);
+
+            ((Point)o).Change(3, 3);
+            Console.WriteLine(o);
+
+            Console.ReadLine();
+        }
+    }
+}
