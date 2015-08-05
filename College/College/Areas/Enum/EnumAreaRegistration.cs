a49fe59eb64b6813946ad5bba2e9020abd0c75bc
rinospam <rino_spam@hotmail.com>
2015-08-05 22:32:50 +0700

    College SP
    Signed-off-by: rinospam <rino_spam@hotmail.com>

---
+++ College/College/Areas/Enum/EnumAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace College.Areas.Enum
+{
+    public class EnumAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Enum";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Enum_default",
+                "Enum/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ College/College/Controllers/DemoController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using College.Models;
+
+namespace College.Controllers
+{
+    public class DemoController : Controller
+    {
+        private CollegeEntities db = new CollegeEntities();
+
+        // GET: /Demo/
+        public ActionResult Index()
+        {
+            //return View(db.Students.ToList());
+            //IEnumerable<Students> model = new st_select_Result 
+           
+            return View(db.st_select(1));
+
+
+
+        }
+
+        // GET: /Demo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            Students students = DbSet<Students>(db.st_select(id)).
+        
+            if (db.st_select(id) == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // GET: /Demo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Demo/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,FirstName")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.st_insert(students.id, students.FirstName);
+                
+
+                //db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(students);
+        }
+
+        // GET: /Demo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: /Demo/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,FirstName")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(students).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(students);
+        }
+
+        // GET: /Demo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: /Demo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Students students = db.Students.Find(id);
+            db.Students.Remove(students);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
